f80de6baeaf9b92241cf03a76848de62b364a18d
anh.tran <anhtpt92@gmail.com>
2021-07-27 10:37:48 +0700

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8dd9f08b45ef5f36117ea2122c75081f5a4935b3',
+    'name' => 'tonytran/oauth2',
+  ),
+  'versions' => 
+  array (
+    'bshaffer/oauth2-server-php' => 
+    array (
+      'pretty_version' => 'v1.11.1',
+      'version' => '1.11.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a0c8000d4763b276919e2106f54eddda6bc50fa',
+    ),
+    'tonytran/oauth2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8dd9f08b45ef5f36117ea2122c75081f5a4935b3',
+    ),
+  ),
+);
